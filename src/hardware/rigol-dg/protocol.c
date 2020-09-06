9f4a2b76c6ef896a0fa45f0fb2151d520618b8c2
Timo Kokkonen <tjko@iki.fi>
2020-09-05 22:06:30 -0700

    Initial Rigol DG1000z driver implementation.

--- src/hardware/rigol-dg/protocol.c
+++ src/hardware/rigol-dg/protocol.c
@@ -18,7 +18,139 @@
  */
 
 #include <config.h>
+#include "scpi.h"
 #include "protocol.h"
+
+
+SR_PRIV const char* waveform_type_to_string(enum waveform_type type)
+{
+	switch (type) {
+	case WF_DC:
+		return "DC";
+	case WF_SINE:
+		return "Sine";
+	case WF_SQUARE:
+		return "Square";
+	case WF_RAMP:
+		return "Ramp";
+	case WF_PULSE:
+		return "Pulse";
+	case WF_NOISE:
+		return "Noise";
+	case WF_ARB:
+		return "Arb";
+		
+	}
+	return "Unknown";
+}
+
+SR_PRIV const struct waveform_spec* get_waveform_spec(const struct channel_spec *ch, enum waveform_type wf)
+{
+	const struct waveform_spec *spec;
+	unsigned int i;
+
+	spec = NULL;
+	for (i = 0; i < ch->num_waveforms; i++) {
+		if (ch->waveforms[i].waveform == wf) {
+			spec = &ch->waveforms[i];
+			break;
+		}
+	}
+	
+	return spec;
+}
+
+SR_PRIV int rigol_dg_get_channel_state(const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
+{
+	struct dev_context *devc;
+	struct sr_channel *ch;
+	struct channel_status *ch_status;
+	const char *command;
+	GVariant *data;
+	const gchar *response, *s;
+	gchar **params;
+	enum waveform_type wf;
+	double freq, ampl, offset, phase;
+	int ret;
+
+	devc = sdi->priv;
+	data = NULL;
+	params = NULL;
+	ret = SR_ERR_NA;
+
+	if (!sdi || !cg)
+		return SR_ERR_BUG;
+
+	ch = cg->channels->data;
+	ch_status = &devc->ch_status[ch->index];
+	sr_info("cg=%s, ch=%s, ch->index=%d", cg->name, ch->name, ch->index);
+
+	command = sr_scpi_cmd_get(devc->cmdset, PSG_CMD_GET_SOURCE);
+	if (command && *command) {
+		ret = sr_scpi_cmd_resp(sdi, devc->device->cmdset,
+				       PSG_CMD_SELECT_CHANNEL, cg->name,
+				       &data, G_VARIANT_TYPE_STRING,
+				       PSG_CMD_GET_SOURCE, cg->name);
+		if (ret != SR_OK)
+			goto done;
+		response = g_variant_get_string(data, NULL);
+		sr_spew("Channel state: '%s'", response);
+		params = g_strsplit(response, ",", 0);
+		if (!params[0]) 
+			goto done;
+		
+		/* First parameter is the waveform type */
+		if (!(s = params[0]))
+			goto done;
+		if (s[0] == '"') s++;
+		sr_info("signal type: %s", s);
+		if (!strncmp(s, "SIN", 3)) { wf = WF_SINE; }
+		else if (!strncmp(s, "SQU", 3)) { wf = WF_SQUARE; }
+		else if (!strncmp(s, "RAMP", 4)) { wf = WF_RAMP; }
+		else if (!strncmp(s, "PULSE", 5)) { wf = WF_PULSE; }
+		else if (!strncmp(s, "NOISE", 5)) { wf = WF_NOISE; }
+		else if (!strncmp(s, "USER", 4)) { wf = WF_ARB; }
+		else if (!strncmp(s, "DC", 2)) { wf = WF_DC; }
+		else goto done;
+		sr_info("waveform: %s (%d)", waveform_type_to_string(wf), wf);
+		ch_status->wf = wf;
+		ch_status->wf_spec = get_waveform_spec(&devc->device->channels[ch->index], wf);
+
+		/* Second parameter if the frequency (or "DEF" if not applicable) */
+		if (!(s = params[1]))
+			goto done;
+		freq = g_ascii_strtod(s, NULL);
+		sr_info("Frequency: %s (%f)", s, freq);
+		ch_status->freq = freq;
+
+		/* Third parameter if the amplitude (or "DEF" if not applicable) */
+		if (!(s = params[2]))
+			goto done;
+		ampl = g_ascii_strtod(s, NULL);
+		sr_info("Amplitude: %s (%f)", s, ampl);
+		ch_status->ampl = ampl;
+
+		/* Fourth parameter if the offset (or "DEF" if not applicable) */
+		if (!(s = params[3]))
+			goto done;
+		offset = g_ascii_strtod(s, NULL);
+		sr_info("Offset: %s (%f)", s, offset);
+		ch_status->offset = offset;
+
+		/* Fifth parameter if the phase (or "DEF" if not applicable) */
+		if (!(s = params[4]))
+			goto done;
+		phase = g_ascii_strtod(s, NULL);
+		sr_info("Phase: %s (%f)", s, phase);
+		ch_status->phase = phase;
+		
+		ret = SR_OK;
+	}
+
+done:
+	g_strfreev(params);
+	return ret;
+}
 
 SR_PRIV int rigol_dg_receive_data(int fd, int revents, void *cb_data)
 {
